5a8e2e290671322b13c37aa49fcc63978367fb89
adamstgr <adam.stgr@gmail.com>
2022-01-08 14:40:31 -0500

    provide scaffolding for user login

---
+++ ComplianceReportGenerator/ComplianceReportGenerator/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ComplianceReportGenerator.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ComplianceReportGenerator.Areas.Identity.IdentityHostingStartup))]
+namespace ComplianceReportGenerator.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ReportDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ReportDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ReportDbContext>();
+            });
+        }
+    }
+}
